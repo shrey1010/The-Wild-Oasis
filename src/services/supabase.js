1185a08cf63d3d7cfaea3be572c29523da163e5a
Shrey Shukla <shreyshukla1010@gmail.com>
2025-01-03 18:29:21 +0530

    fetching data from db

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,8 @@
-//  https://supabase.com/dashboard/project/rftwaywuzaduaicfaubo
-
-
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://rftwaywuzaduaicfaubo.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmdHdheXd1emFkdWFpY2ZhdWJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyODM2ODcsImV4cCI6MjA1MDg1OTY4N30.g6SBAzqZh05S0SffkegizNfgioZ3mgkI-zWnr_hfytI";
-const supabase = createClient(supabaseUrl, supabaseKey); 
 
-export default supabase;  
+export const supabaseUrl = "https://rftwaywuzaduaicfaubo.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmdHdheXd1emFkdWFpY2ZhdWJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyODM2ODcsImV4cCI6MjA1MDg1OTY4N30.g6SBAzqZh05S0SffkegizNfgioZ3mgkI-zWnr_hfytI";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
