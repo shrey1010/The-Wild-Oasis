6c8ce5030fc1635d1b76f83cb7b16e85284e06a9
Shrey Shukla <shreyshukla1010@gmail.com>
2025-01-10 16:21:11 +0530

    ftr - services for authentication

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,3 +1,5 @@
+// https://supabase.com/dashboard/project/rftwaywuzaduaicfaubo/auth/policies
+
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://rftwaywuzaduaicfaubo.supabase.co";
@@ -6,3 +8,13 @@ const supabaseKey =
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+
+
+// import { createClient } from "@supabase/supabase-js";
+
+// export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
